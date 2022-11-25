973b0a693a7f1eb29164157d7d75ac60419d6a38
EstrellaPoot <Estrella.Poot@itksquare.edu.mx>
2022-11-25 13:43:04 -0600

    feat: Add image to force user click

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-let audio = new Audio('sound/TWDOriginalSoundtrack.mp3');
-audio.play();
+let imgZombie = document.querySelector('.img-zombie');
+setTimeout(() => {
+  divZombie.style.backgroundColor = 'rgba(199,85,85, 0.3)';
+  divZombie.style.backgroundImage = 'url(/images/blood.png)';
+  imgZombie.style.transform = 'scale(1)';
+}, 400);
+
+let divZombie = document.querySelector('.div-zombie');
+divZombie.addEventListener('click', () => {
+  divZombie.style.background = 'none';
+  imgZombie.style.transform = 'scale(0)';
+  let audio = new Audio('sound/TWDOriginalSoundtrack.mp3');
+  audio.volume = 0.5;
+  audio.play();
+});
